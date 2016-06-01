aab0a23b15c91cd2267ed9721dfa32628e4ffe41
kimochg <liucheng.cn@outlook.com>
2016-06-02 01:54:57 +0800

    add vote property,js-upvote,js-downvote,js-website-form

--- client/main.js
+++ client/main.js
@@ -5,7 +5,7 @@
 // helper function that returns all available websites
 Template.website_list.helpers({
 	websites:function(){
-		return Websites.find({});
+		return Websites.find({},{sort:{vote:-1}});
 	}
 });
 
@@ -19,7 +19,9 @@ Template.website_item.events({
 		// example of how you can access the id for the website in the database
 		// (this is the data context for the template)
 		var website_id = this._id;
-		console.log("Up voting website with id "+website_id);
+		var web = Websites.findOne({_id:website_id},);
+		increVote(web,1);
+
 		// put the code in here to add a vote to a website!
 
 		return false;// prevent the button from reloading the page
@@ -30,7 +32,8 @@ Template.website_item.events({
 		// (this is the data context for the template)
 		var website_id = this._id;
 		console.log("Down voting website with id "+website_id);
-
+		var web = Websites.findOne({_id:website_id},);
+		increVote(web,-1);
 		// put the code in here to remove a vote from a website!
 
 		return false;// prevent the button from reloading the page
@@ -46,10 +49,61 @@ Template.website_form.events({
 		// here is an example of how to get the url out of the form:
 		var url = event.target.url.value;
 		console.log("The url they entered is: "+url);
+		console.log(url.indexOf("http://"));
 		
-		//  put your website saving code in here!	
+		var title = event.target.title.value;
+		var description = event.target.description.value;
 
+		if (!isURL(url)) {
+			alert("incorrect URL address, Please check.");
+		} else if (!title) {
+			alert("incorrect title, Please check.");
+		} else if (!description) {
+			alert("Your website need a description :)");
+		} else {
+			Websites.insert({
+				title:title, 
+				url:url,
+				description:description,
+				createdOn:new Date(),
+				vote:Websites.find().count
+			});
+		}
 		return false;// stop the form submit from reloading the page
 
 	}
 });
+
+
+function increVote(website, num) {
+	Websites.update({_id:website._id},
+					{$set : {vote:website.vote+num}});
+}
+
+function exchVote(web1, web2) {
+	var tempVote = web1.vote;
+	Websites.update({_id:web1._id},
+					{$set : {vote:web2.vote}});
+	Websites.update({_id:web2._id},
+					{$set : {vote:tempVote}});
+}
+
+function isURL(str_url){ 
+	var strRegex = "^((https|http|ftp|rtsp|mms)?://)"  
+	+ "?(([0-9a-z_!~*'().&=+$%-]+: )?[0-9a-z_!~*'().&=+$%-]+@)?" //ftp的user@  
+	+ "(([0-9]{1,3}\.){3}[0-9]{1,3}" // IP形式的URL- 199.194.52.184  
+	+ "|" // 允许IP和DOMAIN（域名） 
+	+ "([0-9a-z_!~*'()-]+\.)*" // 域名- www.  
+	+ "([0-9a-z][0-9a-z-]{0,61})?[0-9a-z]\." // 二级域名  
+	+ "[a-z]{2,6})" // first level domain- .com or .museum  
+	+ "(:[0-9]{1,4})?" // 端口- :80  
+	+ "((/?)|" // a slash isn't required if there is no file name  
+	+ "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";  
+	var re=new RegExp(strRegex);  
+	est() 
+	if (re.test(str_url)){ 
+	    return (true);  
+	}else{  
+	    return (false);  
+	} 
+} 

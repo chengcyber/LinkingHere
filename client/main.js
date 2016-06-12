a899670c93dfe4b7dbba03a4c8fee82d795341aa
kimochg <liucheng.cn@outlook.com>
2016-06-12 21:41:24 +0800

    Serveral improvements. 90% complete(including HTTP,search) Users can register and login. Users can post new websites if they are logged in. Websites posted by users should have an URL and a description. Users can up and down vote webpages by clicking a plus or a minus button. Websites should be listed with the most up voted site first. The listing page shows when the website was added and how many up and down votes it has. Users can move to a detail page for a website (using routing). On the detail page, users can post comments about a webpage, and they are displayed below the description of the webpage.
    Challenge 1: Automatic information
    
    Can you use the HTTP package for Meteor to pull in information about the
    posted web links automatically, so the user does not need to enter
    anything other than the URL?
    
    Challenge 2: Search function
    
    Implement a search function that allows the user to search within the
    listed sites for key words

--- client/main.js
+++ client/main.js
@@ -1,9 +1,54 @@
+// routing
+
+Router.configure({
+	layoutTemplate: 'ApplicationLayout'
+});
+
+Router.route('/', function() {
+	this.render('navbar', {
+		to : "navbar"
+	});
+	this.render('website_list', {
+		to : "main"
+	});
+});
+
+Router.route('/:id', function(){
+	this.render('navbar', {
+		to : "navbar"
+	});
+	this.render('website_item', {
+		to : "main",
+		data : function() {
+			return Websites.findOne({_id:this.params.id});
+		}
+	});
+	this.render('comments', {
+		to : "forum"
+	});
+});
+
+
+
+
 // accounts-ui config
 Accounts.ui.config(
 	{passwordSignupFields: "USERNAME_AND_EMAIL"}
 	);
 
-
+// infinitescroll
+Session.set("websiteLimit", 10);
+lastScrollTop = 0;
+$(window).scroll(function(event){
+	if ($(window).scrollTop() + $(window).height() > $(document).height() - 100) {
+		var scrollTop = $(this).scrollTop();
+		if (scrollTop > lastScrollTop) {
+			Session.set("websiteLimit", Session.get("websiteLimit") + 5);
+		}
+		lastScrollTop = scrollTop;
+	}
+	// console.log("curLmt "+ Session.get("websiteLimit"));
+});
 
 /////
 // template helpers 
@@ -12,7 +57,14 @@ Accounts.ui.config(
 // helper function that returns all available websites
 Template.website_list.helpers({
 	websites:function(){
-		return Websites.find({},{sort:{vote:-1}});
+		if (Session.get("userFilter")) {
+			return Websites.find({createdBy:Session.get("userFilter")},{sort:{vote:-1,createdOn:-1}});
+		} else if (Session.get("searchFilter")) {
+			Meteor.subscribe("search-websites", Session.get("searchFilter"));
+			return Websites.find({score:{"$exists":true}}, { sort: [["score", "desc"]] });
+		} else {
+			return Websites.find({},{sort:{vote:-1,createdOn:-1}, limit:Session.get("websiteLimit")});
+		}
 	},
 	username : function(){
 		if(Meteor.user()) {
@@ -21,22 +73,135 @@ Template.website_list.helpers({
 		} else {
 			return "anonymous";
 		}
-	}	
+	},
+	getFilterUser : function() {
+		if (Session.get("userFilter")){// they set a filter!
+		  var user = Meteor.users.findOne(
+		    {_id:Session.get("userFilter")});
+		  if (user) {
+		  	return user.username;
+		  }
+		} 
+		else {
+		  return false;
+		}
+	},
+	getSearchFilter : function () {
+		if (Session.get("searchFilter")) {
+			return Session.get("searchFilter");
+		} else {
+			return false;
+		}
+	},
+filtering_websites:function(){
+  if (Session.get("userFilter")){// they set a filter!
+    return true;
+  } 
+  else {
+    return false;
+  }
+},
+searching_websites: function() {
+	if (Session.get("searchFilter")) {
+		return true;
+	} else {
+		return false;
+	}
+}
 });
 
+
+Template.website_item.helpers({
+getUser:function(user_id){
+  var user = Meteor.users.findOne({_id:user_id});
+  if (user){
+    return user.username;
+  }
+  else {
+    return "anon";
+  }
+}
+});
+
+Template.comment.helpers({
+	getUser :function(user_id) {
+		var user = Meteor.users.findOne({_id:user_id});
+		if (user) {
+			return user.username;
+		} else {
+			return "anon";
+		}
+	}
+});
+
+Template.comments.helpers({
+	comments : function() {
+		// console.log(Websites.findOne({_id:Session.get("websiteDetailID")}));
+		var website = Websites.findOne({_id:Session.get("websiteDetailID")});
+		var comments = website.comments;
+		var author = website.commentBy;
+		var result = new Array();
+		for (var i = 0,l = comments.length; i < l; i++) {
+			result.push({comment : comments[i],
+						commentBy : author[i]
+							})
+		}
+		return result;
+	}
+});
 
 /////
 // template events 
 /////
 
+
+Template.navbar.events({
+	'submit .js-set-search-website' : function(event){
+		var searchStr = event.target.searchStr.value;
+		console.log("searching:"+searchStr);
+		Session.set("searchFilter", searchStr);
+		return false;
+	}
+});
+
+Template.website_list.events({
+	"click .js-unset-website-filter": function(event){
+		Session.set("userFilter", undefined);
+	},
+	'click .js-unset-search-website' : function(event){
+		Session.set("searchFilter", undefined);
+	}
+});
+
 Template.website_item.events({
 	"click .js-upvote":function(event){
 		// example of how you can access the id for the website in the database
 		// (this is the data context for the template)
-		var website_id = this._id;
-		var web = Websites.findOne({_id:website_id},);
-		increVote(web,1);
+		if (Meteor.user()) {
+			// console.log("press like button");
+			var website_id = this._id;
+			var web = Websites.findOne({_id:website_id},);
+			var curUserId = Meteor.user()._id;
 
+			if (!web.wholike.some(function (item, index, array) {
+				return (item == curUserId);
+			})) {
+				Websites.update({_id:web._id},
+							{$inc : {vote:1}, $push: {wholike: curUserId}}
+						);
+				// console.log("like +1")
+				if (web.whodislike.some(function (item, index, array) {
+					return (item == curUserId)
+				})) {
+					Websites.update({_id:web._id},
+							{$inc : {vote:1}, $pullAll : {whodislike: [curUserId]}}
+						)
+					// console.log("dislike to like +1");
+				}
+			}
+		} else {
+			alert("Please login to vote!");
+		}
 		// put the code in here to add a vote to a website!
 
 		return false;// prevent the button from reloading the page
@@ -45,15 +210,41 @@ Template.website_item.events({
 
 		// example of how you can access the id for the website in the database
 		// (this is the data context for the template)
-		var website_id = this._id;
-		console.log("Down voting website with id "+website_id);
-		var web = Websites.findOne({_id:website_id},);
-		increVote(web,-1);
+		if (Meteor.user()) {
+			// console.log("press dislike button");
+			var website_id = this._id;
+			// console.log(this);
+			var web = Websites.findOne({_id:website_id},);
+			var curUserId = Meteor.user()._id;
+			if (!web.whodislike.some(function (item, index, array) {
+				return (item == curUserId);
+			})) {
+				Websites.update({_id:web._id},
+							{$inc : {vote:-1}, $push: {whodislike: curUserId}}
+						);
+				// console.log("dislike -1");
+				if (web.wholike.some(function (item, index, array) {
+					return (item == curUserId)
+				})) {
+					Websites.update({_id:web._id},
+								{$inc : {vote:-1}, $pullAll : {wholike : [curUserId]}})
+					// console.log("like to dislike -1");
+				}
+			}
+		} else {
+			alert("Please login to vote!");
+		}
 		// put the code in here to remove a vote from a website!
 
 		return false;// prevent the button from reloading the page
+	},
+	"click .js-set-website-filter": function(event) {
+		Session.set("userFilter", this.createdBy);
+	},
+	"click .js-set-website-detail" : function(event) {
+		Session.set("websiteDetailID",this._id);
 	}
-})
+});
 
 Template.website_form.events({
 	"click .js-toggle-website-form":function(event){
@@ -81,11 +272,68 @@ Template.website_form.events({
 				url:url,
 				description:description,
 				createdOn:new Date(),
-				vote:Websites.find().count
+				vote:0,
+				createdBy:Meteor.user()._id,
+				wholike:new Array(),
+				whodislike:new Array(),
+				comments: new Array(),
+				commentBy: new Array()
 			});
+			$("#website_form").toggle('slow');
 		}
+		console.log("added ok");
 		return false;// stop the form submit from reloading the page
 
+	},
+	"click .js-http-autofill" : function(event) {
+		var url = document.getElementById("url").value;
+
+		if (!isURL(url)) {
+			alert("incorrect URL address, Please check.");
+		} else {
+			Meteor.call('remoteHttpGet', url,{
+			// options
+			},function(err,res){
+				if (err) {
+					console.log(err);
+				} else {
+					// console.log(res.content);
+					var title = getTitle(res.content);
+					var description = getDescription(res.content);
+					console.log(title);
+					console.log(description);
+					document.getElementById("title").value = title;
+					document.getElementById("description").value = description;
+					// $("#website_form").title.value = title;
+					// console.log(getDescription(res.content));
+					// console.log("****************");
+					// console.log(getTitle(res.content));
+				}
+			})
+		}
+		
+	}
+});
+
+Template.comment_form.events({
+	"submit .js-save-comment-form" : function(event) {
+		var comment = event.target.comment.value;
+		if (comment) {
+			// console.log("comment: "+comment);
+			var authorID = Meteor.user()._id;
+			// console.log(authorID);
+			var website = Websites.findOne({_id:Session.get("websiteDetailID")});
+			// console.log(Session.get("websiteDetailID"));
+			
+			Websites.update({_id:Session.get("websiteDetailID")},
+							{$push : {comments:comment, commentBy:authorID}});
+			// console.log(website);
+
+		} else {
+			alert("write something please.");
+		}
+
+		return false;// stop the form submit from reloading the page
 	}
 });
 
@@ -94,18 +342,6 @@ Template.body.helpers({
 }
 )
 
-function increVote(website, num) {
-	Websites.update({_id:website._id},
-					{$set : {vote:website.vote+num}});
-}
-
-function exchVote(web1, web2) {
-	var tempVote = web1.vote;
-	Websites.update({_id:web1._id},
-					{$set : {vote:web2.vote}});
-	Websites.update({_id:web2._id},
-					{$set : {vote:tempVote}});
-}
 
 function isURL(str_url){ 
 	var strRegex = "^((https|http|ftp|rtsp|mms)?://)"  
@@ -119,10 +355,30 @@ function isURL(str_url){
 	+ "((/?)|" // a slash isn't required if there is no file name  
 	+ "(/[0-9a-z_!~*'().;?:@&=+$,%#-]+)+/?)$";  
 	var re=new RegExp(strRegex);  
-	est() 
 	if (re.test(str_url)){ 
 	    return (true);  
 	}else{  
 	    return (false);  
 	} 
-} 
+}
+
+function getTitle(str_html) {
+	var re = /<title>.*<\/title>/;
+	if (str_html.search(re) > 0) {
+		return str_html.match(re)[0].toString().replace(/<\/?title>/ig,"");
+	} else {
+		return "";
+	}
+}
+
+function getDescription(str_html) {
+	var re1 = /<meta[ ]+name=["|']description["|'][ ]+content=['|"]/i;
+	var re2 = /['|"][ ]*\/>$/i;
+	// console.log(str_html.search(re1));
+	if (str_html.search(re1) > 0) {
+		var str = str_html.match(/<meta[ ]+name=["|']description["|'].*/i)[0];
+		return str.replace(re1, "").replace(re2, "");
+	} else {
+		return "";
+	}
+}
--- server/main.js
+++ server/main.js
@@ -1,34 +1,80 @@
 Meteor.startup(function () {
-// code to run on server at startup
-if (!Websites.findOne()){
-	console.log("No websites yet. Creating starter data.");
-	  Websites.insert({
-		title:"Goldsmiths Computing Department", 
-		url:"http://www.gold.ac.uk/computing/", 
-		description:"This is where this course was developed.", 
-		createdOn:new Date(),
-		vote:0
-	});
-	 Websites.insert({
-		title:"University of London", 
-		url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route", 
-		description:"University of London International Programme.", 
-		createdOn:new Date(),
-		vote:0
-	});
-	 Websites.insert({
-		title:"Coursera", 
-		url:"http://www.coursera.org", 
-		description:"Universal access to the world’s best education.", 
-		createdOn:new Date(),
-		vote:0
-	});
-	Websites.insert({
-		title:"Google", 
-		url:"http://www.google.com", 
-		description:"Popular search engine.", 
-		createdOn:new Date(),
-		vote:0
-	});
-}
+	// code to run on server at startup
+	Websites._ensureIndex({
+		"title" : "text",
+		"url" : "text",
+		"description" : "text"
+		});	
+	seed();
 });
+Meteor.publish("search-websites", function(searchField){
+	console.log("Searching for ", searchField);
+	return Websites.find({$text : { $search : searchField}},
+	{
+		fields : {
+			score : {$meta : "textScore"}
+		},
+		sort : {
+			score : {$meta : "textScore"}
+		}
+	});
+});
+Meteor.methods({
+	'remoteHttpGet' : function(url, options) {
+		return HTTP.get(url, options);
+	}
+});
+
+function seed() {
+	if (!Websites.findOne()){
+		console.log("No websites yet. Creating starter data.");
+		  Websites.insert({
+			title:"Goldsmiths Computing Department", 
+			url:"http://www.gold.ac.uk/computing/", 
+			description:"This is where this course was developed.", 
+			createdOn:new Date(),
+			createdBy:"admin",
+			vote:0,
+			wholike:new Array(),
+			whodislike:new Array(),
+			comments: new Array(),
+			commentBy: new Array()
+		});
+		 Websites.insert({
+			title:"University of London", 
+			url:"http://www.londoninternational.ac.uk/courses/undergraduate/goldsmiths/bsc-creative-computing-bsc-diploma-work-entry-route", 
+			description:"University of London International Programme.", 
+			createdOn:new Date(),
+			createdBy:"admin",
+			vote:0,
+			wholike:new Array(),
+			whodislike:new Array(),
+			comments: new Array(),
+			commentBy: new Array()
+		});
+		 Websites.insert({
+			title:"Coursera", 
+			url:"http://www.coursera.org", 
+			description:"Universal access to the world’s best education.", 
+			createdOn:new Date(),
+			createdBy:"admin",
+			vote:0,
+			wholike:new Array(),
+			whodislike:new Array(),
+			comments: new Array(),
+			commentBy: new Array()
+		});
+		Websites.insert({
+			title:"Google", 
+			url:"http://www.google.com", 
+			description:"Popular search engine.", 
+			createdOn:new Date(),
+			createdBy:"admin",
+			vote:0,
+			wholike:new Array(),
+			whodislike:new Array(),
+			comments: new Array(),
+			commentBy: new Array()
+		});
+	}
+}
